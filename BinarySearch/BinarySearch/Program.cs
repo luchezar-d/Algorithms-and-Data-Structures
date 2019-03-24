264e64646962e9f04ca70b15ee6b48163a83c902
Luchezar <Luchezar.d.dimitrov@gmail.com>
2019-03-25 00:15:41 +0200

    Added Binary Search and Bubble Sort Algorithms

---
+++ BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,48 @@
+﻿
+namespace BinarySearch
+{
+    using System;
+
+    class Program
+    {
+        public static int SearchBinary(int[] numbers, int searchValue)
+        {
+            int midIndex;
+            int leftIndex = 0;
+            int rightIndex = numbers.Length - 1;
+            
+            while (leftIndex <= rightIndex)
+            {
+                midIndex = leftIndex + ((rightIndex - leftIndex) / 2);
+
+                if (numbers[midIndex] == searchValue)
+                {
+                    return midIndex;
+                }
+                else
+                {
+                    if (numbers[midIndex] > searchValue)
+                    {
+                        rightIndex = midIndex - 1;
+                    }
+                    else
+                    {
+                        leftIndex = midIndex + 1;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            Console.WriteLine("Enter the number you want to search from 1 to 13 : ");
+            int searchNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Value found at index : " + SearchBinary(numbers, searchNumber));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        public static void BubbleSort(int[] arr)
+        {
+            int temp = 0;
+
+            for (int write = 0; write < arr.Length; write++)
+            {
+                for (int sort = 0; sort < arr.Length - 1 - write; sort++)
+                {
+                    if (arr[sort] > arr[sort + 1])
+                    {
+                        temp = arr[sort + 1];
+                        arr[sort + 1] = arr[sort];
+                        arr[sort] = temp;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 3 numbers to sort : ");
+
+            int[] arr = new int[3];
+
+            for (int i = 0; i < 3; i++)
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+
+            BubbleSort(arr);
+
+            for (int i = 0; i < arr.Length; i++) Console.Write(arr[i] + " ");
+
+            Console.ReadKey();
+        }
+    }
+}
